a679fac6cf2f33baf3cdb7f626bfc9bf7b26dd70
Churta11 <churta11@wgu.edu>
2025-12-14 13:51:20 -0600

    - Implemented three unit tests: train model test, compute metrics test, and save/load test - Included a screenshot of succesful unit test on screenshot folder

--- test_ml.py
+++ test_ml.py
@@ -1,28 +1,49 @@
 import pytest
-# TODO: add necessary import
+from train_model import train_model
+from ml.model import compute_model_metrics, save_model, load_model
+from sklearn.ensemble import RandomForestClassifier
+import numpy as np
 
 # TODO: implement the first test. Change the function name and input as needed
-def test_one():
+def test_train_model():
     """
-    # add description for the first test
+    # Returns trained model
     """
-    # Your code here
+    X_train = np.random.rand(20,5)
+    y_train = np.random.randint(0, 2, 20)
+    model = train_model(X_train, y_train)
+    assert isinstance(model, RandomForestClassifier)
     pass
 
 
 # TODO: implement the second test. Change the function name and input as needed
-def test_two():
+def test_compute_metrics():
     """
-    # add description for the second test
+    # Test computed model metrics
     """
-    # Your code here
+    y_true = [1,0,1,1]
+    y_preds = [0, 1, 1, 0]
+    fbeta, precision, recall = compute_model_metrics(y_true, y_preds)
     pass
 
 
 # TODO: implement the third test. Change the function name and input as needed
-def test_three():
+def test_save_load_model(tmp_path):
     """
-    # add description for the third test
+    # Test creates file, saves file and ensures file loads
     """
-    # Your code here
+    X_train = np.random.rand(20,5)
+    y_train = np.random.randint(0, 2, 20)
+    model = RandomForestClassifier()
+    model.fit(X_train, y_train)
+
+    model_path = tmp_path / 'model.pkl'
+
+    save_model(model, model_path)
+
+    assert model_path.exists()
+
+    loaded_model = load_model(model_path)
+    
+    assert isinstance(loaded_model, RandomForestClassifier)
     pass
